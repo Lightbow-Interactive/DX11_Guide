b0fbf104b8d3dbfe53cdfa531e4ecddc6a4103f6
Pascal Gluth <pascal.gluth@gmx.net>
2022-04-15 11:35:34 +0200

    Creating the Window

--- DX11_Guide/DX11_Guide.cpp
+++ DX11_Guide/DX11_Guide.cpp
@@ -1,8 +1,29 @@
-#include <iostream>
+#include <SDL.h>
 
-int main()
+SDL_Window* window;
+char windowTitle[] = "SDL Window";
+int width = 960;
+int height = 540;
+
+int main(int argv, char** args)
 {
-	std::cout << "Hello World" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
+    window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    SDL_SetWindowResizable(window, SDL_FALSE);
 
-	return 0;
+    bool run = true;
+    while (run)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                run = false;
+        }
+
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
